027352a45a01df53045e2a7bb013400ffa8d1915
Ethel <etheltibeb@gmail.com>
2025-02-18 19:38:39 +0200

    Docker and Flask /api: serve our model via Flask. This file will be responsible for loading the trained models and creating endpoints for predictions.

---
+++ fraud_detection_api/serve_model.py
@@ -0,0 +1,28 @@
+# serve_model.py
+#
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the pre-trained models
+model_fraud_detection = joblib.load('Fraud__Detection_RandomForest_best_model.pkl')
+model_credit_card_fraud = joblib.load('Credit_Card_Fraud_RandomForest_best_model.pkl')
+
+# API endpoint for fraud detection model
+@app.route('/predict_fraud', methods=['POST'])
+def predict_fraud():
+    data = request.get_json()
+    prediction = model_fraud_detection.predict([data['features']])
+    return jsonify({"prediction": prediction[0]})
+
+# API endpoint for credit card fraud detection model
+@app.route('/predict_credit_card_fraud', methods=['POST'])
+def predict_credit_card_fraud():
+    data = request.get_json()
+    prediction = model_credit_card_fraud.predict([data['features']])
+    return jsonify({"prediction": prediction[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
